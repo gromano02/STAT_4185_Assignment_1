fcb1efbdd8b46605905c45b6848df0cce283d96d
Giovanni <giovanniromano@live.com>
2023-10-09 01:41:31 -0400

    Decrypt Two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,9 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+decrypted_message = list(encrypted_message)
+n = len(decrypted_message)
+for i in range(1, n//2, 2):
+    decrypted_message[i], decrypted_message[n-i-1] = decrypted_message[n-i-1], decrypted_message[i]
+x = "".join(decrypted_message)
+print(x)
